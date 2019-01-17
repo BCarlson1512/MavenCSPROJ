80ec16fe7435fc4784bb203d82aca4376770267b
Benpr <Benpr@DESKTOP-3OBV20F>
2019-01-17 13:53:41 -0500

    Minor tweaks to instructions window

--- Project2.0/src/main/java/InstructionsWindow.java
+++ Project2.0/src/main/java/InstructionsWindow.java
@@ -10,20 +10,45 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class InstructionsWindow extends JFrame {
+	
+	String [] instructionsText = new String[5];{
+		instructionsText[0] = "Game Rules 1: \n"
+				+ "\n"
+				+ "\n"
+				+ "This game is like chess but with a twist \n"
+				+ "\n"
+				+ "You move your players until you try to contest  \n"
+				+ "a square, at that point it becomes a battle  \n"
+				+ "for who keeps their piece \n"
+				+ "\n"
+				+ "To win the game, you need to clear the board of AI pokemon\n"
+				+ "\n"
+				+ "Losing is caused when your pokemon dies\n"
+				+ "\n"
+				+ "";
+		
+	instructionsText[1] = "Game Rules 2: \n"
+			+ "Each Pokemon has its own unique strengths/weaknesses, choose wisely \n"
+			+ "\n"
+			+ "Custom pokemon coming soon"
+			+ "\n"
+			+ "Click Button to go to page 3" ;
+	
+	instructionsText[2] = "Game Rules 3: \n"
+			+ "Frequently Asked Questions:"
+			+ "\n"
+			+ ""
+			+ "" ;
+	
+	}
+	
+	
+	
 	public InstructionsWindow() {
 		
 		JTextArea instructionsArea = new JTextArea();
 		
-		instructionsArea.setText("Game Rules: \n"
-								+ "This game is like chess but with a twist \n"
-								+ "\n"
-								+ "You move your players until you try to contest  \n"
-								+ "a square, at that point it becomes a battle  \n"
-								+ "for who keeps their piece \n"
-								+ "\n"
-								+ "To win the game, you need to clear the board of AI pokemon\n"
-								+ "\n"
-								+ "Losing is caused when your pokemon dies\n");
+		instructionsArea.setText(instructionsText[0]);
 		
 		JLabel lblInstructions = new JLabel("Instructions");
 		lblInstructions.setFont(new Font("Lucida Sans", Font.BOLD, 17));
